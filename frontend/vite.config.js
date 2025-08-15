3d228b5da3af92100e76ce8415c36be0380d1d04
Ningthouremba <ningthouremba@gmail.com>
2025-08-16 04:47:59 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173, allowedHosts: ["tambola-frontend-ycaj.onrender.com"],
+   }, 
   plugins: [react(), tailwindcss()],
 })
